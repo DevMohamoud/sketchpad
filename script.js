e1962eccdcc765dd5d69149fce173b6c63818964
devmohamoud <devmohamoud@gmail.com>
2024-07-29 15:59:33 -0700

    Created two functions, (1) creates single square, (2) generates require numbers of squares

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-console.log('Testing');
 
+function createSquare() {
+	const square = document.createElement('div');
+	square.classList.add('square-wrapper');
+	return square;
+};
+
+function generateSquares(getByElementTag, sizeOfSquares) {
+	const container = document.getElementById(getByElementTag);
+	if (container) {
+		for (let i=0; i < sizeOfSquares * sizeOfSquares; i++) {
+			const square = createSquare();
+			container.appendChild(square);
+		}
+	} else {
+		console.error(`requested container "${getByElementTag}" not found!`);
+	}
+};
+
+generateSquares('container', 16);
